8f0a2c53575719b3fcdd6132e454c91edd9104c2
kevinlee075 <kevinlee075@gmail.com>
2021-06-06 18:51:12 +0800

    Create Unit_1_problem_sets.py

---
+++ Unit_1_problem_sets.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 15:00:04 2021
+
+@author: Lenovo
+"""
+
+s = str(input('s = '))
+Vowels = 0
+for letter in s:
+  if letter == 'a' or letter == 'e' or letter == 'i' \
+     or letter == 'o' or letter == 'u':
+     Vowels += 1
+print('Number of vowels: ' + str(Vowels))
