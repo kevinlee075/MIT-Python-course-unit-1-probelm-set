dd3a8036677c3befe694dbdfdafec8ab101e2f81
kevinlee075 <kevinlee075@gmail.com>
2021-06-06 19:06:45 +0800

    Update Unit_1_problem_sets.py

--- Unit_1_problem_sets.py
+++ Unit_1_problem_sets.py
@@ -5,10 +5,10 @@ Created on Sun Jun  6 15:00:04 2021
 @author: Lenovo
 """
 
-s = str(input('s = '))
-Vowels = 0
-for letter in s:
+s = str(input('s = ')) #define s as an input string
+Vowels = 0 #initial vowel
+for letter in s: #examine whether s has vowels using loop
   if letter == 'a' or letter == 'e' or letter == 'i' \
-     or letter == 'o' or letter == 'u':
-     Vowels += 1
-print('Number of vowels: ' + str(Vowels))
+     or letter == 'o' or letter == 'u': 
+     Vowels += 1 #count vowels
+print('Number of vowels: ' + str(Vowels)) #finally print number of vowels
